aef1d635107d29df4adcbbf179e9e22de6bc2a73
KamyarBrk <krberenjkar@gmail.com>
2025-09-27 15:14:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-5-nano", temperature=0)
-resp = llm.invoke("Hello from inside the Dev Container!")
-print(resp.content)
+
+
+prompt = ChatPromptTemplate.from_template(
+    "You are an AI model that performs the enumeration phase of a penetration test."
+    "\nQuestion: {question}\nAnswer:"
+)
+
+
+llm = ChatOpenAI(model="gpt-5-nano",temperature=0)
+
+llm_chain = prompt | llm | StrOutputParser()
+
+def query_llm(question): 
+    print(llm_chain.invoke({'question': question})) 
+
+print("Welcome to the Enumeration Phase")
+while True:
+    user = input("Enter-> ")
+    query_llm(user)
