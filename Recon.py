1cd87ff520522290e913ba0baf7ac16c26749a74
Paul-Dean <paulfd554@gmail.com>
2025-10-09 11:39:55 -0400

    Changed code from API to local ollama environment

--- Recon.py
+++ Recon.py
@@ -1,18 +1,15 @@
 
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
+from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os
-load_dotenv()
 
 
 #Initializes the prompt for the chatbot, establishing it is a recon module and sets place for user input
 init = ChatPromptTemplate.from_template("You are an AI model that performs the reconnaissance phase of a penetration test."
     "\nQuestion: {question}\nAnswer:")
 
-#Initializes the AI with api key and model
-Reco = ChatOpenAI(api_key=os.environ.get("OPENAI_API_KEY"), model='gpt-5-nano')
+Reco = OllamaLLM(model="gemma3:1b")
 
 #Chains the prompt with the initialized prompt, ai, and an output parser for formatting
 recon_chain = init | Reco | StrOutputParser()
