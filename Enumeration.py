660c70bbd53294303226e23ff2336c8f8419874e
Alden <alden.erazo@gmail.com>
2025-10-16 13:19:18 -0400

    Added an exit function

--- Enumeration.py
+++ Enumeration.py
@@ -11,15 +11,22 @@ prompt = ChatPromptTemplate.from_template(
 )
 
 
-enum = OllamaLLM(model="gemma3:1b")
+enum = OllamaLLM(model="gemma3:4b")
 
 llm_chain = prompt | enum | StrOutputParser()
 
 def query_llm(question): 
     print(llm_chain.invoke({'question': question})) 
 
+# --- ENUMERATION START ---
+
 print("Welcome to the Enumeration Phase")
 while True:
+# Gets user input
     user = input("Enter-> ")
+# If user enters 'exit' then the model is not queried and the program terminates. 
+    if user.lower() == "exit":
+        print('Goodbye!')
+        break
+# Queries the LLM with user input
     query_llm(user)
-
