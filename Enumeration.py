0896b370ef9dd722f14b032e0bedc6e76741f4b4
KamyarBrk <krberenjkar@gmail.com>
2025-10-25 13:49:34 -0400

    added subgraph

--- Enumeration.py
+++ Enumeration.py
@@ -2,8 +2,18 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langgraph.graph import START, StateGraph
+from langgraph.checkpoint.memory import InMemorySaver
+from typing import TypedDict
 
 
+class State(TypedDict):
+    foo: str
+
+# Subgraph
+
+def subgraph_node_1(state: State):
+    return {"foo": state["foo"] + "bar"}
 
 prompt = ChatPromptTemplate.from_template(
     "You are an AI model that performs the enumeration phase of a penetration test."
@@ -21,6 +31,21 @@ def query_llm(question):
 # --- ENUMERATION START ---
 
 print("Welcome to the Enumeration Phase")
+
+subgraph_builder = StateGraph(State)
+subgraph_builder.add_node(subgraph_node_1)
+subgraph_builder.add_edge(START, "subgraph_node_1")
+subgraph = subgraph_builder.compile()  
+
+# Parent graph
+
+builder = StateGraph(State)
+builder.add_node("node_1", subgraph)  
+builder.add_edge(START, "node_1")
+
+checkpointer = InMemorySaver()
+graph = builder.compile(checkpointer=checkpointer)  
+
 while True:
 # Gets user input
     user = input("Enter-> ")
